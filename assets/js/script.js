770e1e082458a85fe6c80cc309b28927a2dd26a6
you know who <chorvickprocode@gmail.com>
2021-01-12 15:03:24 -0500

    made array to hold questions - placeholders to start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,73 @@
 /// js will go here
+var data = [
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+    {
+        question: "a  question",
+        answers: ["ans1", "ans2", "ans3", "ans4"],
+        rightAnswer: "ans2"
+    },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
