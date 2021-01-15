b5a418c96f29df27443ae6e60f3d589372805791
you know who <chorvickprocode@gmail.com>
2021-01-14 23:15:53 -0500

    added comments of what needs to be done in order

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,7 +53,31 @@ var data = [
 ];
 
 
+//// set a timer to zero
+///  set score to zero
+//// need a counter to count how many times we go through or when at end of questions
+//// change time when button is clicked to 100
 
+///write some functions 
+
+//// need function to put the questions up or into an element and write to screen 
+//// function to listen ?? 
+
+/// need function to hide answers not selected
+/// keep track of score after question , deduct timer if question wrong ect....
+
+/// need a function to change to the next question
+/// write new/next question to screen
+
+/// loop through all questions or until timer is zero ---penalty -10 for wrong ans
+
+/// once game over clear screen display final score - 
+
+///now timer fuction is finished --- timer must be kept running all the while since start til now
+
+/// ask user for initials - test for initials use JSON to store locally
+//// display hi scores , if a button clicked clear score(s) and local storage
+/// if play again is slected start timer and roll back up top
 
 
 
