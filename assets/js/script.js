708ba00916393379ab329ee3a3144d9f1ed86193
you know who <chorvickprocode@gmail.com>
2021-01-18 22:39:32 -0500

    made function for after questions are answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#start");
 var datadiv = document.querySelector("#datadiv");
 var container = document.querySelector("#container");
-var secondsLeft = 120;
+var secondsLeft = 100;
 var holdInterval = 0;
 var ulWrite = document.createElement("ul");
 var dataIndex = 0;
@@ -119,8 +119,21 @@ function compare(event) {
         }
     }
     qindex++;
+    if (qindex >= data.length) {
+        alldone();
+        createDiv.textContent = "Your answered " + score + " out of ten questions correct !!";
 
+    } else {
+        render(qindex);
 
+    }
+    datadiv.appendChild(createDiv);
+
+}
+
+function alldone() {
+    datadiv.innerHTML = "";
+    currentTime.innerHTML = "";
 }
 
 
@@ -128,39 +141,12 @@ function compare(event) {
 
 
 
-//datadiv.appendChild(createDiv);
 
 
 
 
 
 
-
-//// set a timer to zero
-///  set score to zero
-//// need a counter to count how many times we go through or when at end of questions
-//// change time when button is clicked to 100
-
-///write some functions 
-
-//// need function to put the questions up or into an element and write to screen 
-//// function to listen ?? 
-
-/// need function to hide answers not selected
-/// keep track of score after question , deduct timer if question wrong ect....
-
-/// need a function to change to the next question
-/// write new/next question to screen
-
-/// loop through all questions or until timer is zero ---penalty -10 for wrong ans
-
-/// once game over clear screen display final score - 
-
-///now timer fuction is finished --- timer must be kept running all the while since start til now
-
-/// ask user for initials - test for initials use JSON to store locally
-//// display hi scores , if a button clicked clear score(s) and local storage
-/// if play again is slected start timer and roll back up top
 
 
 
